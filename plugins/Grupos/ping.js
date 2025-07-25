658997621cfee7aa2303aa6ade4f52565cd64f9b
AngelDown12 <angeldown12xz@gmail.com>
2025-07-24 23:47:02 -0600

    Actualizar ping.js

--- plugins/Grupos/ping.js
+++ plugins/Grupos/ping.js
@@ -30,7 +30,7 @@ let texto = `*🚀 𝐕𝐞ff𝐥𝐨𝐜𝐢𝐝𝐚𝐝*
 *💻 𝐒𝐞𝐫𝐯𝐢𝐝𝐨𝐫 / nodovip*
 • *Ram:* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
-conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: '𝙀𝙡𝙞𝙩𝙚𝘽𝙤𝙩𝙂𝙡𝙤𝙗𝙖𝙡', thumbnailUrl: 'https://qu.ax/YQAhw.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
+conn.sendMessage(m.chat, { text: texto, contextInfo: { externalAdReply: { title: '', body: '𝘾𝙧𝙞𝙨𝙩𝙞𝙖𝙣 𝘽𝙤𝙩 𝙀𝙡𝙞𝙩𝙚', thumbnailUrl: 'https://files.catbox.moe/3h4fzb.jpg', sourceUrl: '', mediaType: 1, renderLargerThumbnail: true }}})
 
 }
 handler.help = ['ping']
