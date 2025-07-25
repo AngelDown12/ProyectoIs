26ad4db06240be77c655ae4da4d9a786ef84bf94
AngelDown12 <angeldown12xz@gmail.com>
2025-07-25 00:01:54 -0600

    Actualizar Menuanimes.js

--- plugins/Menus/Menuanimes.js
+++ plugins/Menus/Menuanimes.js
@@ -84,7 +84,7 @@ let menu = `
 
  `.trim()
     
-const vi = ['https://telegra.ph/file/c411d252f7eb27b2b7324.mp4']
+const vi = ['']
 
 try {
 await conn.sendMessage(m.chat, { video: { url: vi.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak }) 
